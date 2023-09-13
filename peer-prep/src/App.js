fa1c4bba45e5ec32630ea98d42a74495d1ea82e6
Aishwarya Nair <aishrn1202@gmail.com>
2023-09-13 14:04:05 +0800

    add log in and sign up page

--- peer-prep/src/App.js
+++ peer-prep/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { LoginSignUp } from './Components/LoginSignUp/LoginSignUp';
 
 function App() {
   return (
     <div>
-      <LoginSignUp></LoginSignUp>
+      <LoginSignUp/>
     </div>
   );
 }
--- peer-prep/src/Components/LoginSignUp/LoginSignUp.jsx
+++ peer-prep/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 import './LoginSignUp.css'
+import {useState} from 'react'
+
+import password_icon from '../Assets/password.png'
+import user_icon from '../Assets/user.png'
 
 export const LoginSignUp = () => {
+    const [action, setAction] = useState("Log In");
   return (
-    <div>LoginSignUp</div>
+    <div className="container">
+        <div className="header">
+            <div className="text"> {action} </div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            <div className="input">
+                <img src= {user_icon} alt="" />
+                <input type="text" placeholder = "Username"/>
+            </div>
+            <div className="input">
+                <img src= {password_icon} alt="" />
+                <input type="password" placeholder = "Password"/>
+            </div>
+        </div>
+        {action === "Sign Up" ? <div></div>: <div className="forgot-password"> Forgot Password? <span> Click Here.</span></div>}
+        <div className="submit-container">
+            <div className={action==="Sign Up"? "submit gray": "submit"} 
+            onClick = {() => {setAction("Log In")}}>Log In</div>
+            <div className={action==="Log In"? "submit gray": "submit"} 
+            onClick = {() => {setAction("Sign Up")}}>Sign Up</div>
+        </div>
+    </div>
+    
   )
 }
+
+
