5b40cf0ba2503df239350f482f55448cbaebd2d5
Aishwarya Nair <aishrn1202@gmail.com>
2023-09-13 16:40:06 +0800

    add navbar

--- peer-prep/src/App.js
+++ peer-prep/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { LoginSignUp } from './Components/LoginSignUp/LoginSignUp';
+import { Navbar } from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <LoginSignUp/>
     </div>
   );
---
+++ peer-prep/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navbar.css'
+
+export const Navbar = () => {
+  return (
+    <nav className = "nav">
+       <a href = "#" className = "logo"> <span>&lt;Peer/</span> Prep&gt;</a>
+    </nav>
+  )
+}
