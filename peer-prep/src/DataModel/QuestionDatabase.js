458b8507a9778fabb2f0b8c1f2d2f517513db0c5
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2023-09-15 21:06:32 +0800

    Add QuestionDatabase Model
    Add a Data Model that encapsulates the Question Database.

---
+++ peer-prep/src/DataModel/QuestionDatabase.js
@@ -0,0 +1,55 @@
+import QuestionModel from "./QuestionModel";
+
+class QuestionDatabase {
+    constructor() {
+        this.database = new Map();
+    }
+
+    addQuestion(questionToAdd) {
+        if (!this.isDuplicateQuestion(questionToAdd)) {
+            this.database.set(questionToAdd.id, questionToAdd);
+        }
+    }
+
+    deleteQuestion(questionId) {
+        if (this.database.has(questionId)) {
+            this.database.delete(questionId);
+        }
+    }
+
+    updateQuestion(questionToUpdate) {
+        this.database.set(questionToUpdate.id, questionToUpdate);
+    }
+
+    getAllQuestions() {
+        var questionList = [];
+
+        for (const [key, value] of this.database) {
+            questionList.push(value);
+        }
+
+        return questionList;
+    }
+
+    isDuplicateQuestion(questionToCheck) {
+        if (this.database.has(questionToCheck.id)) {
+            return true;
+        }
+
+        questionList = this.getAllQuestions();
+
+        for (var i = 0; i < questionList.length; i++) {
+            if (questionToCheck.title === questionList[i].title) {
+                return true;
+            }
+
+            if (questionToCheck.description === questionList[i].description) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+export default QuestionDatabase;
