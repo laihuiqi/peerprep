ad8958acb13e89f2ab33664583d81544b573c61a
Linda124 <lindatom1@MacBook-Pro-3.local>
2023-09-27 12:27:55 +0800

    Add match button and popup

---
+++ peer-prep/src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { MatchButton } from './Matching/MatchButton';
+
+function App() {
+  return (
+    <div>
+      <MatchButton/>
+    </div>
+  );
+}
+
+export default App;
