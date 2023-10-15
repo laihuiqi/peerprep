daa1ce4dd16015980d19b6873a8b5c0a2498f095
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2023-10-16 00:35:58 +0800

    Update User Data Model

--- UserService/models/user.js
+++ UserService/models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  _id: String,
-  name: String,
-  email: String,
-  githubId: String,
-  preferredLanguage: String,
+  mongo_id: mongoose.Schema.Types.ObjectId,
+  _id: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  githubId: { type: String, required: true },
+  preferredLanguage: { type: String, required: true },
   isAdmin: Boolean,
 });
 
