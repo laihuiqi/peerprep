ca4d76bf2763b3f695c2ec8f8d630ce3cdea1690
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2023-10-18 23:54:24 +0800

    Enhance Abstraction
    Abstracted Database Logic from Routes

--- UserService/routes/users.js
+++ UserService/routes/users.js
@@ -1,51 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/user");
+const {
+  getAllUsers,
+  getUserById,
+  addUserToDatabase,
+  updateUserData,
+  deleteUserFromDatabase,
+} = require("../controllers/userDatabaseController");
 
-router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "GET USERS",
-  });
-});
+router.get("/", getAllUsers);
 
-router.post("/", (req, res, next) => {
-  res.status(201).json({
-    message: "POST USERS",
-  });
-});
+router.get("/:userId", getUserById);
 
-router.get("/:userId", (req, res, next) => {
-  const id = req.params.userId;
+router.post("/:userId", addUserToDatabase);
 
-  res.status(200).json({
-    message: "GET USER",
-    userId: id,
-  });
-});
+router.patch("/:userId", updateUserData);
 
-router.post("/:userId", (req, res, next) => {
-  const id = req.params.userId;
-
-  const user = {
-    userId: id,
-    name: req.body.name,
-    email: req.body.email,
-  };
-
-  res.status(201).json({
-    message: "POST USER",
-    userData: user,
-  });
-});
-
-router.delete("/:userId", (req, res, next) => {
-  const id = req.params.userId;
-
-  res.status(200).json({
-    message: "DELETE USER",
-    userId: id,
-  });
-});
+router.delete("/:userId", deleteUserFromDatabase);
 
 module.exports = router;
