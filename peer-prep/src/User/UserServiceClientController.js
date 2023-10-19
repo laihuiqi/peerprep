3bb49ba456bc7eda5f9f488952add24bea06fefd
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2023-10-19 16:23:39 +0800

    Add Client Side User Service Controller
    To facilitate communication between Client and User Service. This is however hidden from Client, and the Client is provided a more abstract API to use

---
+++ peer-prep/src/User/UserServiceClientController.js
@@ -0,0 +1,77 @@
+// Interface Between User Service and Client
+
+import axios from "axios";
+
+const USER_SERVICE_HOST = "http://localhost:3000/";
+
+async function getUserData(userId) {
+  try {
+    const result = await axios.get(USER_SERVICE_HOST + "users/" + userId);
+
+    return result;
+  } catch (error) {
+    console.log("Error" + error);
+
+    return null;
+  }
+}
+
+async function postUserData(
+  userId,
+  userName,
+  userEmail,
+  userGithubId,
+  userPreferredLanguage
+) {
+  try {
+    const result = await axios.post(USER_SERVICE_HOST + "users/" + userId, {
+      name: userName,
+      email: userEmail,
+      githubId: userGithubId,
+      preferredLanguage: userPreferredLanguage,
+    });
+
+    return result;
+  } catch (error) {
+    console.log("Error" + error);
+
+    return null;
+  }
+}
+
+async function patchUserData(
+  userId,
+  userName,
+  userEmail,
+  userGithubId,
+  userPreferredLanguage
+) {
+  try {
+    const result = await axios.patch(USER_SERVICE_HOST + "users/" + userId, {
+      name: userName,
+      email: userEmail,
+      githubId: userGithubId,
+      preferredLanguage: userPreferredLanguage,
+    });
+
+    return result;
+  } catch (error) {
+    console.log("Error" + error);
+
+    return null;
+  }
+}
+
+async function deleteUserData(userId) {
+  try {
+    const result = await axios.delete(USER_SERVICE_HOST + "users/" + userId);
+
+    return result;
+  } catch (error) {
+    console.log("Error" + error);
+
+    return null;
+  }
+}
+
+export { getUserData, postUserData, patchUserData, deleteUserData };
