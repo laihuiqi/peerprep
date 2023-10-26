fed23c206642a7f03b2f936f14f2f8b0d51ed107
Linda124 <lindatom1@MacBook-Pro-3.local>
2023-10-26 16:39:18 +0800

    Revert "Integrate matching ui changes"
    This reverts commit b4e33c7d66a5ba28c4e35bf89561b33340ceddb9, reversing
    changes made to bbaf26879ea351d76891520b3d1e2b691dd824e8.

--- peer-prep/src/App.js
+++ peer-prep/src/App.js
@@ -3,14 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import { LoginSignUp } from './Components/LoginSignUp/LoginSignUp';
 import { Navbar } from './Components/Navbar/Navbar';
-<<<<<<< HEAD
 import { Questions } from './Components/QuestionList/QuestionList';
 import CollaborationWindow from './Components/Collaboration/CollaborationWindow';
 
-=======
-import { Questions } from './Components/QuestionList/QuestionList'; 
-import { MatchButton } from './Components/Matching/MatchButton';
->>>>>>> add-matching-ui
 
 function App() {
   return (
@@ -19,16 +14,10 @@ function App() {
       <Navbar/>
       <Routes>
       {/* <LoginSignUp/> */}
-<<<<<<< HEAD
       <Route path = "/" element = {<Questions/>}/>
        {/* Adding CollaborationWindow for testing */}
        <Route path="/collaboration" element={<CollaborationWindow />}/>
        </Routes>
-=======
-      <MatchButton/>
-      <Questions/>
-
->>>>>>> add-matching-ui
     </div>
     </Router>
   );
