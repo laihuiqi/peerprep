b6cf9fd16f309f1ea68610a448947573c0c7ec22
Aishwarya Nair <aishrn1202@gmail.com>
2023-10-31 00:16:10 +0800

    improve UI

--- peer-prep/src/Components/QuestionList/Question.jsx
+++ peer-prep/src/Components/QuestionList/Question.jsx
@@ -2,102 +2,64 @@ import React, {useState} from 'react'
 import './Question.css'
 import delete_icon from '../Assets/bin.png'
 import edit_icon from '../Assets/pencil.png'
+import {QuestionEdit} from "./QuestionEdit"
 
 export const Question = ({question, i, deleteQuestion, updateQuestion}) => {
     const [selected, setSelected] = useState(null);
-    const [title, setTitle] = useState(question.title);
-    const [difficulty, setDifficulty] = useState(question.complexity);
-    const [topic, setTopic] = useState(question.category);
-    const [description, setDescription] = useState(question.description)
-
     const toggle = (i) => {
         if (!isEdit) {
-            if(selected === i) {
-                return setSelected(null)
-            }
-            setSelected(i)
+            selected === i ? setSelected(null) : setSelected(i)
         }
     };
     const [isEdit, setEdit] = useState(false);
     let tagClass = "q-tag";
 
-    if(question.complexity.toLowerCase() === "easy") {
-        tagClass += " q-tag-green"
-    } else if (question.complexity.toLowerCase() === "medium") {
-        tagClass += " q-tag-orange"
-    } else if (question.complexity.toLowerCase() === "hard"){
-        tagClass += " q-tag-red"
-    } else {
-        tagClass += " q-tag-white"
+    if (question.complexity) {
+        if(question.complexity.toLowerCase() === "easy") {
+            tagClass += " q-tag-green"
+        } else if (question.complexity.toLowerCase() === "medium") {
+            tagClass += " q-tag-orange"
+        } else if (question.complexity.toLowerCase() === "hard"){
+            tagClass += " q-tag-red"
+        } else {
+            tagClass += " q-tag-white"
+        }
     }
   return (
-    <div className= {isEdit ? "form-container": "question-container"}>
-        <div className={isEdit ? "container": "question"}>
-            <div className="q-header" onClick = {() => toggle(i)}>
-                <div className="q-name">
-                    <div className={isEdit ? "q-form-id": "q-id"}>#{i + 1}</div>
-                    {isEdit === true? <input type="text" className="q-form-title q-form-input" defaultValue = {question.title}
-                    onChange = {(e) => {setTitle(e.target.value)}}/> : 
-                    <div className={selected === i ? "q-title-show": "q-title"}>{question.title}</div>}
+    <div>
+        {isEdit? <QuestionEdit q = {question} index = {i} updateQ = {updateQuestion} setEdit = {setEdit}/>:
+        <div className= "question-container">
+            <div className="question">
+                <div className="q-header" onClick = {() => toggle(i)}>
+                    <div className="q-name">
+                        <div className="q-id">#{i + 1}</div>
+                        <div className={selected === i ? "q-title-show": "q-title"}>{question.title}</div>
+                    </div>
+                    <span> {selected === i ? "-" : "+"}</span>
                 </div>
-                <span> {selected === i ? "-" : "+"}</span>
+                
+                <div className="q-content">
+                    <div className= {selected === i ? "q-content-expand": "q-content-contract"}>
+                            <div className="q-tag-container">
+                                <div className="q-tags">
+                                    <div className= {tagClass}>{question.complexity}</div>
+                                    <div className="q-tag">{question.category}</div>
+                                </div>
+                                <div className="q-edit" onClick = {()=> setEdit(true)}>
+                                    <img src = {edit_icon} alt=""/>
+                                </div>
+                            </div>
+
+                            <div className="q-description">{question.description}</div>
+                    </div>
+                </div>
+                
             </div>
-            
-            {!isEdit 
-                ? 
-                <div className= {selected === i ? "q-content-show": "q-content"}>
-                    <div className="q-tag-container">
-                        <div className={isEdit ? "q-form-tags" : "q-tags"}>
-                            {isEdit === true? <input type="text" className="q-form-tag q-form-input" defaultValue = {question.complexity} 
-                            onChange = {(e) => {setDifficulty(e.target.value)}}/> : 
-                            <div className= {tagClass}>{question.complexity}</div>}
-                            {isEdit === true? <input type="text" className="q-form-tag q-form-input" defaultValue = {question.category}
-                            onChange = {(e) => {setTopic(e.target.value)}}/> : 
-                            <div className="q-tag">{question.category}</div>}
-                        </div>
-                        <div className="q-edit" onClick = {()=> setEdit(true)}>
-                            <img src = {edit_icon} alt=""/>
-                        </div>
-                    </div>
-
-                    {isEdit === true? <textarea type="text" className="q-form-description q-form-input" defaultValue = {question.description}
-                    onChange = {(e) => {setDescription(e.target.value)}}/> : 
-                    <div className="q-description">{question.description}</div>}
-                    
-                    {isEdit === true? <button className="submit-btn" 
-                    onClick = {(e) => {
-                        setEdit(false);
-                        updateQuestion(question.id, title, description, difficulty, topic)
-                        }}>Submit</button> : <div></div>}
-                </div>
-                : 
-                <div className="q-tag-container">
-                    <div className={isEdit ? "q-form-tags" : "q-tags"}>
-                        {isEdit === true? <input type="text" className="q-form-tag q-form-input" defaultValue = {question.complexity} 
-                        onChange = {(e) => {setDifficulty(e.target.value)}}/> : 
-                        <div className= {tagClass}>{question.complexity}</div>}
-                        {isEdit === true? <input type="text" className="q-form-tag q-form-input" defaultValue = {question.category}
-                        onChange = {(e) => {setTopic(e.target.value)}}/> : 
-                        <div className="q-tag">{question.category}</div>}
-                    </div>
-                </div>}
-
-            {isEdit === true? <textarea type="text" className="q-form-description q-form-input" defaultValue = {question.description}
-            onChange = {(e) => {setDescription(e.target.value)}}/> : null}
-            
-            {isEdit === true? 
-            <div className="btn-container">
-                <button className="submit-btn" 
-            onClick = {(e) => {
-                setEdit(false);
-                updateQuestion(question.id, title, description, difficulty, topic)
-                }}>Submit</button> </div> : null}
-            
+            <div className="delete-btn" onClick = {(e) => deleteQuestion(question._id)}>
+                <img src= {delete_icon} alt="" />
+            </div>
         </div>
-        <div className="delete-btn" onClick = {(e) => deleteQuestion(question.id)}>
-            <img src= {delete_icon} alt="" />
-        </div>
+        }
     </div>
-    
-  )
+    )
 }
--- peer-prep/src/Components/QuestionList/QuestionForm.jsx
+++ peer-prep/src/Components/QuestionList/QuestionForm.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import './QuestionForm.css'
 import delete_icon from '../Assets/bin.png'
+import { QDifficultyDropdown } from './QDifficultyDropdown'
 
 export const QuestionForm = ({qId, addQuestion, setAddQ, setQId, questionNumber}) => {
   const [title, setTitle] = useState("");
-  const [difficulty, setDifficulty] = useState("");
+  const [difficulty, setDifficulty] = useState("easy");
   const [topic, setTopic] = useState("");
-  const [description, setDescription] = useState("")
+  const [description, setDescription] = useState("");
 
   const handleSubmit = (toggleAddQ, addQ) => e => {
     e.preventDefault();
@@ -15,6 +16,11 @@ export const QuestionForm = ({qId, addQuestion, setAddQ, setQId, questionNumber}
     const response = addQ(title, difficulty, topic, description);
     return response; // If array length is 0, then successful, else can index through it for errors
   }
+
+  function handleChosenDifficulty(e) {
+    return setDifficulty(e.target.value)
+  }
+
   return (
     <div className="form-container">
       <form className = "container" onSubmit={handleSubmit(setAddQ, addQuestion)}>
@@ -26,29 +32,29 @@ export const QuestionForm = ({qId, addQuestion, setAddQ, setQId, questionNumber}
           }/>
         </div>
         
+        <div className="q-form-content">
           <div className="q-form-tags">
-            <input type="text" className="q-form-tag q-form-input" 
-            placeholder = 'Difficulty'
-            onChange = {
-              (e) => setDifficulty(e.target.value)
+                <QDifficultyDropdown chosenDifficulty = {difficulty} setDifficulty = {handleChosenDifficulty}/>
+                <input type="text" className="q-form-tag q-form-input" 
+                placeholder = 'Topic'
+                onChange = {
+                  (e) => setTopic(e.target.value)
+                }
+                />
+            
+              </div>
+              <textarea type="text" className="q-form-description q-form-input" 
+              placeholder = "Question description"
+              onChange = {
+                (e) => setDescription(e.target.value)
             }/>
-            <input type="text" className="q-form-tag q-form-input" 
-            placeholder = 'Topic'
-            onChange = {
-              (e) => setTopic(e.target.value)
-            }
-            />
-          </div>
-          <textarea type="text" className="q-form-description q-form-input" 
-          placeholder = "Question description"
-          onChange = {
-            (e) => setDescription(e.target.value)
-          }/>
-          <div className="btn-container">
-            <button type = "cancel" className="cancel-btn" onClick = {(e)=> {setAddQ(false); setQId(qId - 1)}}>Cancel</button>
-            <button type = "submit" className="submit-btn">Submit</button>
-          </div>
-          
+
+              <div className="btn-container">
+                <button type = "cancel" className="cancel-btn" onClick = {(e)=> {setAddQ(false); 
+                  setQId(qId - 1)}}>Cancel</button>
+                <button type = "submit" className="submit-btn">Submit</button>
+              </div>
+        </div>
       </form>
       <div className="ghost-btn">
         <img src= {delete_icon} alt=""/>
