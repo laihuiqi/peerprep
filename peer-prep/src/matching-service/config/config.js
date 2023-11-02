a8320badbe994f1e33957782c9277a47ee9c8712
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2023-11-03 00:14:59 +0800

    Update config.js
    To Run on Docker

--- peer-prep/src/matching-service/config/config.js
+++ peer-prep/src/matching-service/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    mongodbUri: 'mongodb://127.0.0.1:27017/peer-prep',
-    //mongodbUri: 'mongodb://mongo_db:27017/peer-prep',
-    rabbitmqUrl: 'amqp://127.0.0.1:5672',
-    //rabbitmqUrl: 'amqp://rabbitmq:5672'
+  // mongodbUri: 'mongodb://127.0.0.1:27017/peer-prep',
+  mongodbUri: "mongodb://mongo_db:27017/peer-prep",
+  // rabbitmqUrl: 'amqp://127.0.0.1:5672',
+  rabbitmqUrl: "amqp://rabbitmq:5672",
 };
