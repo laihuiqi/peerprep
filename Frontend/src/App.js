e49083f64cf049c9af188ed02725f58d56d30f83
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2023-11-09 14:23:11 +0800

    Add Route Protection

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,45 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+} from "react-router-dom";
+
 import { LoginSignUp } from "./Components/LoginSignUp/LoginSignUp";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { Questions } from "./Components/QuestionList/QuestionList";
 import { LandingPage } from "./Components/LandingPage/LandingPage";
 import CollaborationWindow from "./Components/Collaboration/CollaborationWindow";
+
+import { isUserAdmin } from "./User/UserState";
+import { isUserLoggedIn } from "./Authentication/AuthenticationState";
+import useUserStatusHook from "./CustomHooks/userHook";
+
+import "./App.css";
+
+// import { auth } from "./Authentication/firebase";
+// import { useAuthState } from "react-firebase-hooks/auth";
+
+const ProtectedRoute = ({
+  checkAdminStatus,
+  redirectPath = "/login",
+  children,
+}) => {
+  const userStatus = useUserStatusHook(isUserLoggedIn, isUserAdmin);
+
+  if (userStatus.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!userStatus.isLoggedIn || (checkAdminStatus && !userStatus.isAdmin)) {
+    return <Navigate to={redirectPath} replace />;
+  }
+
+  return children ? children : <Outlet />;
+};
 
 function App() {
   return (
@@ -13,12 +47,30 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/questions" element={<Questions />} />
-          {/* Adding CollaborationWindow for testing */}
           <Route path="/login" element={<LoginSignUp />} />
-          <Route path="/collaboration" element={<CollaborationWindow />} />
-          <Route path="*" element={<p>Nothing To Prep Here Peer: 404!</p>} />
+
+          <Route element={<ProtectedRoute checkAdminStatus={false} />}>
+            <Route path="/landing" element={<LandingPage />} />
+            <Route path="/collaboration" element={<CollaborationWindow />} />
+            <Route
+              path="/unauthorized"
+              element={<p>Peer, You Should'nt Be Here: 401!</p>}
+            />
+            <Route path="*" element={<p>Peer, Nothing To Prep Here: 404!</p>} />
+          </Route>
+
+          <Route
+            path="/questions"
+            element={
+              <ProtectedRoute
+                redirectPath="/unauthorized"
+                checkAdminStatus={true}
+                // isAllowed={!!isUserAdmin() && !!isUserLoggedIn()}
+              >
+                <Questions />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </Router>
