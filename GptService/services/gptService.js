5d85277098110b6e4987a8706b3115da5f70c407
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2023-11-11 22:13:34 +0800

    Update History Service
    - Increase Connection Timeout Time to 10s

--- GptService/services/gptService.js
+++ GptService/services/gptService.js
@@ -1,70 +1,67 @@
-const config = require('../config/config');
-const { OpenAI } = require('openai');
+const config = require("../config/config");
+const { OpenAI } = require("openai");
 
 const gpt = new OpenAI({ apiKey: config.API_KEY });
 
 let sessionCache = new Map();
 
 const getResponse = async (userId, prompt) => {
-    try {
-      const response = await gpt.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prompt }],
-        temperature: 1,
-        max_tokens:200
-        });
-        
-      const answer = response.choices[0].message.content;
+  try {
+    const response = await gpt.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      temperature: 1,
+      max_tokens: 200,
+    });
 
-      cacheToSession(userId, prompt, answer);
+    const answer = response.choices[0].message.content;
 
-      console.log(`Received response: ${answer}`);
+    cacheToSession(userId, prompt, answer);
 
-      return { status: 'success', reply: answer };
+    console.log(`Received response: ${answer}`);
 
-    } catch (error) {
-        console.log('Error getting response from GPT-3: ', error);
-        return { status: 'error', message: error };
-    }
-}
+    return { status: "success", reply: answer };
+  } catch (error) {
+    console.log("Error getting response from GPT-3: ", error);
+    return { status: "error", message: error };
+  }
+};
 
 const cacheToSession = (userId, prompt, reply) => {
-    console.log(`Caching to session for ${userId}`);
+  console.log(`Caching to session for ${userId}`);
 
-    if (!sessionCache.has(userId)) {
-        const record = [{ prompt: prompt, reply: reply }];
-        sessionCache.set(userId, record);
+  if (!sessionCache.has(userId)) {
+    const record = [{ prompt: prompt, reply: reply }];
+    sessionCache.set(userId, record);
+  } else {
+    const record = sessionCache.get(userId);
+    const currRecord = { prompt, reply };
 
-    } else {
-        const record = sessionCache.get(userId);
-        const currRecord = { prompt, reply };
-
-        record.push(currRecord);
-        sessionCache.set(userId, record);
-    }
-}
+    record.push(currRecord);
+    sessionCache.set(userId, record);
+  }
+};
 
 const getCache = (userId) => {
-    console.log(`Getting cache for ${userId}`);
+  console.log(`Getting cache for ${userId}`);
 
-    if (!sessionCache.has(userId)) {
-        return { status: 'error', message: 'No session found for this user.' };
-
-    } else {
-        const record = sessionCache.get(userId);
-        return { status: 'success', record: record };
-    }
-}
+  if (!sessionCache.has(userId)) {
+    return { status: "error", message: "No session found for this user." };
+  } else {
+    const record = sessionCache.get(userId);
+    return { status: "success", record: record };
+  }
+};
 
 const exitSession = (userId) => {
-    console.log(`Exiting session for ${userId}`);
+  console.log(`Exiting session for ${userId}`);
 
-    sessionCache.delete(userId);
-    return true;
-}
+  sessionCache.delete(userId);
+  return true;
+};
 
 module.exports = {
-    getResponse,
-    getCache,
-    exitSession
-}
+  getResponse,
+  getCache,
+  exitSession,
+};
