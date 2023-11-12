39548aa3849b3fa56ad7256d4ca8c69bb17c39e8
Linda124 <lindatom1@MacBook-Pro-3.local>
2023-11-13 07:58:47 +0800

    Backup script to run questions

---
+++ QuestionService/populateDb.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Question = require('./models/questionModel'); 
+const config = require("./config/config");
+
+const questionsData = [
+  {
+    title: 'Test Question 1',
+    description: 'This is for Test Question 1',
+    complexity: 'Easy',
+    category: 'Data Structures',
+    language: 'C++',
+  },
+  {
+    title: 'Test Question 2',
+    description: 'This is for Test Question 2',
+    complexity: 'Medium',
+    category: 'Arrays',
+    language: 'Java',
+  }
+];
+
+async function populateDB() {
+  try {
+    await mongoose.connect(config.mongodbUri);
+    await Question.deleteMany({});
+    const createdQuestions = await Question.insertMany(questionsData);
+    console.log('Questions successfully added to database:', createdQuestions);
+  } catch (error) {
+    console.error('Unable to populate database:', error);
+  } 
+}
+
+populateDB();
