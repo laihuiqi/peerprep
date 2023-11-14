11d7ec00c120320e372021d8fde70b31868edc55
Aishwarya Nair <aishrn1202@gmail.com>
2023-11-14 20:46:13 +0800

    fix user edit profile function call bug

--- Frontend/src/Components/LandingPage/LandingPage.jsx
+++ Frontend/src/Components/LandingPage/LandingPage.jsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import {useState} from 'react'
-import './LandingPage.css'
-import {Dashboard} from './Dashboard'
-import {UserProfile} from './UserProfile'
-import { AttemptHistory } from './AttemptHistory'
-import {NavigationPanel} from './NavigationPanel'
+import React from "react";
+import {useState} from "react";
+import "./LandingPage.css";
+import {Dashboard} from "./Dashboard";
+import {UserProfile} from "./UserProfile";
+import {AttemptHistory} from "./AttemptHistory";
+import {NavigationPanel} from "./NavigationPanel";
+import {getUserName} from "../../User/UserState";
 
- 
 export const LandingPage = () => {
-    const [page_name, setPageName] = useState("Dashboard");
-    const [isList, setIsList] = useState(true)
+ const [page_name, setPageName] = useState("Dashboard");
+ const [isList, setIsList] = useState(true);
+ const [username, setUsername] = useState(getUserName());
 
-  return (
-    <div className = "landing-container">
-        <NavigationPanel setPageName = {setPageName} setIsList = {setIsList}/>
-        
-        <div className="page-view">
-            <div className="page-heading">
-            {page_name}
-            </div>
-            {page_name === "Dashboard" ? <Dashboard/> : <div></div>}
-            {page_name === "Attempt History" ? <AttemptHistory isList = {isList} setIsList = {setIsList}/> : <div></div>}
-            {page_name === "User Profile" ? <UserProfile/> : <div></div>}
-        </div>
-    </div>
-  )
-}
+ return (
+  <div className="landing-container">
+   <NavigationPanel
+    setPageName={setPageName}
+    setIsList={setIsList}
+    userName={username}
+   />
+
+   <div className="page-view">
+    <div className="page-heading">{page_name}</div>
+    {page_name === "Dashboard" ? <Dashboard /> : <div></div>}
+    {page_name === "Attempt History" ? (
+     <AttemptHistory isList={isList} setIsList={setIsList} />
+    ) : (
+     <div></div>
+    )}
+    {page_name === "User Profile" ? (
+     <UserProfile setUsername={setUsername} />
+    ) : (
+     <div></div>
+    )}
+   </div>
+  </div>
+ );
+};
