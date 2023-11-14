970b82fc5958cf9a52a458d876deca056e97afcb
Aishwarya Nair <aishrn1202@gmail.com>
2023-11-14 19:32:42 +0800

    push updated version

--- Frontend/src/Components/LoginSignUp/SignUp.jsx
+++ Frontend/src/Components/LoginSignUp/SignUp.jsx
@@ -11,111 +11,123 @@ import {registerUser} from "../../User/UserServiceAPI";
 const MISSING_FIELD_ERROR_MESSAGE = "Please fill all fields!";
 
 export const SignUp = ({setAction, setError}) => {
-	const navigate = useNavigate();
+ const navigate = useNavigate();
 
-	const [userEmail, setUserEmail] = useState("");
-	const [userPassword, setUserPassword] = useState("");
-	const [userName, setUserName] = useState("");
-	const [preferredLang, setPreferredLang] = useState("");
+ const [userEmail, setUserEmail] = useState("");
+ const [userPassword, setUserPassword] = useState("");
+ const [userName, setUserName] = useState("");
+ const [preferredLang, setPreferredLang] = useState("");
 
-	const isEmpty = (str) => {
-		return str === "";
-	};
+ const isEmpty = (str) => {
+  return str === "";
+ };
 
-	const handleSubmit = async () => {
-		if (isEmpty(userEmail) || isEmpty(userPassword) || isEmpty(userName)) {
-			setError(MISSING_FIELD_ERROR_MESSAGE);
-		} else {
-			const result = await registerUser(
-				userName,
-				userEmail,
-				userPassword,
-				"GitHub ID Placeholder",
-				preferredLang
-			);
+ const handleSubmit = async () => {
+  if (isEmpty(userEmail)  || isEmpty(userPassword) || isEmpty(userName)) {
+   setError(MISSING_FIELD_ERROR_MESSAGE);
+  } else {
+   const result = await registerUser(
+    userName,
+    userEmail,
+    userPassword,
+    "GitHub ID Placeholder",
+    preferredLang
+   );
 
-			if (result.success) {
-				navigate("/landing");
-			} else {
-				console.log(result);
-				setError(result.message);
-			}
-		}
-	};
+   if (result.success) {
+    navigate("/landing");
+   } else {
+    console.log(result);
+    setError(result.message);
+   }
+  }
+ };
 
-	return (
-		<div className="signup-container">
-			<div className="inputs">
-				<div className="input">
-					<div className="input-container">
-						<img src={user_icon} alt="" />
-						<input
-							type="text"
-							placeholder="Email"
-							onChange={(e) => {
-								setUserEmail(e.target.value);
-							}}
-							value={userEmail}
-						/>
-					</div>
-				</div>
-				<div className="input">
-					<div className="input-container">
-						<img src={password_icon} alt="" />
-						<input
-							type="password"
-							placeholder="Password"
-							onChange={(e) => {
-								setUserPassword(e.target.value);
-							}}
-							value={userPassword}
-						/>
-					</div>
-				</div>
-				<div className="input">
-					<input
-						type="text"
-						placeholder="Name"
-						onChange={(e) => {
-							setUserName(e.target.value);
-						}}
-						value={userName}
-					/>
-				</div>
-				<div className="input">
-					<input
-						type="text"
-						placeholder="Preferred programming language"
-						onChange={(e) => {
-							setPreferredLang(e.target.value);
-						}}
-						value={preferredLang}
-					/>
-				</div>
-			</div>
+ return (
+  <div className="signup-container">
+   <div className="inputs">
+    <div className="input">
+     <div className="input-container">
+      <img src={user_icon} alt="" />
+      <input
+       type="text"
+       placeholder="Email"
+       onChange={(e) => {
+        setUserEmail(e.target.value);
+       }}
+       value={userEmail}
+      />
+     </div>
+    </div>
+    <div className="input">
+     <div className="input-container">
+      <img src={password_icon} alt="" />
+      <input
+       type="password"
+       placeholder="Password"
+       onChange={(e) => {
+        setUserPassword(e.target.value);
+       }}
+       value={userPassword}
+      />
+     </div>
+    </div>
+    <div className="input">
+     <input
+      type="text"
+      placeholder="Name"
+      onChange={(e) => {
+       setUserName(e.target.value);
+      }}
+      value={userName}
+     />
+    </div>
+    <div className="input">
+     {/* <input
+      type="text"
+      placeholder="Preferred programming language"
+      onChange={(e) => {
+       setPreferredLang(e.target.value);
+      }}
+      value={preferredLang}
+     /> */}
+     <select
+      value={preferredLang.toLowerCase()}
+      onChange={(e) => {
+       setPreferredLang(e.target.value);
+      }}
+      className="signup-language-dropdown"
+     >
+      <option value="python">Python</option>
+      <option value="cpp">C++</option>
+      <option value="java">Java</option>
+      <option value="javascript">Javascript</option>
+     </select>
+    </div>
+   </div>
 
-			<div className="submit-container">
-				<div
-					className="submit"
-					onClick={(e) => {
-						handleSubmit();
-					}}
-				>
-					Sign Up
-				</div>
-			</div>
-			<div className="login-nav">
-				Already have an account?
-				<span
-					onClick={() => {
-						setError("");
-						setAction("Log In");
-					}}
-				>
-					{" "}
-					Log In.
-				</span>
-			</div>
-		</div>
-	);
+   <div className="submit-container">
+    <div
+     className="submit"
+     onClick={(e) => {
+      handleSubmit();
+     }}
+    >
+     Sign Up
+    </div>
+   </div>
+   <div className="login-nav">
+    Already have an account?
+    <span
+     onClick={() => {
+      setError("");
+      setAction("Log In");
+     }}
+    >
+     {" "}
+     Log In.
+    </span>
+   </div>
+  </div>
+ );
 };
