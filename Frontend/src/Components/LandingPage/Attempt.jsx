a428b90528f82a054e870e9779ee1156a06f8e3c
Aishwarya Nair <aishrn1202@gmail.com>
2023-11-15 19:00:37 +0800

    add fix for history service

--- Frontend/src/Components/LandingPage/Attempt.jsx
+++ Frontend/src/Components/LandingPage/Attempt.jsx
@@ -1,30 +1,48 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Attempt.css";
 import Utility from "../../Utility/Utility";
+import axios from "axios";
+import {QUESTION_SERVICE_URL} from "../QuestionList/config";
 
-export const Attempt = ({ i, attempt, setSelectedAttempt, setIsList }) => {
-  let tagClass = Utility.setDifficultyTag("user-q-tag", attempt.complexity);
+export const Attempt = ({i, attempt, setSelectedAttempt, setIsList}) => {
+	let tagClass = Utility.setDifficultyTag("user-q-tag", attempt.complexity);
 
-  console.log(attempt);
-  
-  return (
-    <div
-      className="attempt-container"
-      onClick={() => {
-        setSelectedAttempt(attempt);
-        setIsList(false);
-      }}
-    >
-      <div className="attempt-name">
-        <div className="attempt-id"> #{i + 1} </div>
-        <div className="attempt-title">{attempt.title}</div>
-      </div>
-      <div className="attempt-tags">
-        {/* to be replaced with date passed as component parameter */}
-        <div className="user-q-tag attempt-date">31/10/2023</div>
-        <div className={tagClass}>{attempt.complexity}</div>
-        <div className="user-q-tag">{attempt.category}</div>
-      </div>
-    </div>
-  );
+	const [attemptDetails, setAttempt] = useState({});
+	const fetchQuestion = async (id) => {
+		try {
+			let response;
+
+			response = await axios.get(QUESTION_SERVICE_URL + "/" + id);
+			console.log(response);
+			if (response.status === 200) {
+				return response.data;
+			} else {
+				console.error("Error fetching questions:", response);
+				return {};
+			}
+		} catch (error) {
+			console.error("Error fetching questions:", error);
+		}
+	};
+
+	return (
+		<div
+			className="attempt-container"
+			onClick={() => {
+				// setSelectedAttempt(attempt);
+				// setIsList(false);
+			}}
+		>
+			<div className="attempt-name">
+				<div className="attempt-id"> #{i + 1} </div>
+				<div className="attempt-title">{attempt.questionId}</div>
+			</div>
+			<div className="attempt-tags">
+				{/* to be replaced with date passed as component parameter */}
+				<div className="user-q-tag attempt-date">31/10/2023</div>
+				<div className={tagClass}>{attempt.complexity}</div>
+				<div className="user-q-tag">{attempt.category}</div>
+			</div>
+		</div>
+	);
 };
--- Frontend/src/Components/LandingPage/AttemptHistory.jsx
+++ Frontend/src/Components/LandingPage/AttemptHistory.jsx
@@ -1,14 +1,15 @@
-import React, {useState, useEffect} from 'react'
-import {Attempt} from "./Attempt"
-import {AttemptView} from "./AttemptView"
-import './AttemptHistory.css'
-import { getUserAttempts } from '../../History/HistoryServiceAPI'
-import {getUserId} from "../../User/UserState"
+import React, {useState, useEffect} from "react";
+import {Attempt} from "./Attempt";
+import {AttemptView} from "./AttemptView";
+import "./AttemptHistory.css";
+import {getUserAttempts} from "../../History/HistoryServiceAPI";
+import {getUserId} from "../../User/UserState";
 
 export const AttemptHistory = ({isList, setIsList}) => {
-    const [attempts, setAttempts] = useState([]);
-    const [selectedAttempt, setSelectedAttempt] = useState(null);
+	const [attempts, setAttempts] = useState([]);
+	const [selectedAttempt, setSelectedAttempt] = useState(null);
 
+<<<<<<< Updated upstream
     // to be replaced with actual attempt fetch
     const fetchAttempts = async () => {
         setAttempts(getUserAttempts(getUserId()));
@@ -32,3 +33,38 @@ export const AttemptHistory = ({isList, setIsList}) => {
     </div>
   )
 }
+=======
+	// to be replaced with actual attempt fetch
+	const fetchAttempts = async () => {
+		const results = await getUserAttempts(getUserId());
+		setAttempts(results);
+		console.log("Attempts");
+		console.log(attempts);
+	};
+
+	useEffect(() => {
+		fetchAttempts();
+	}, []);
+
+	return (
+		<div className="attempt-history-container">
+			{isList ? (
+				<div className="attempt-list-container">
+					{/* to be replaced by mapping over actual stored attempts */}
+					{attempts.map((a, index) => (
+						<Attempt
+							key={index}
+							attempt={a}
+							i={index}
+							setSelectedAttempt={setSelectedAttempt}
+							setIsList={setIsList}
+						/>
+					))}
+				</div>
+			) : (
+				<AttemptView attempt={selectedAttempt} setIsList={setIsList} />
+			)}
+		</div>
+	);
+};
+>>>>>>> Stashed changes
