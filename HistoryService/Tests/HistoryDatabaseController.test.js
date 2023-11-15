50f69cb6ac95d19123eb6ff87a6e564c85e4e0e5
Jai2501 <52578354+Jai2501@users.noreply.github.com>
2023-11-15 21:47:59 +0800

    Update Test Cases

--- HistoryService/Tests/HistoryDatabaseController.test.js
+++ HistoryService/Tests/HistoryDatabaseController.test.js
@@ -13,7 +13,11 @@ test("Add Attempt Details To Database", async () => {
     "sampleUserId1",
     "sampleUserId2",
     "sampleSessionId",
-    "sampleQuestionId"
+    "sampleQuestionId",
+    "Sample Question Title",
+    "Sample Question Description",
+    "Sample Question Category",
+    "Sample Question Complexity"
   );
 
   expect(result[0].affectedRows).toEqual(1);
@@ -26,4 +30,12 @@ test("Get Attempt Details From Database", async () => {
   expect(result[0][0]["userId2"]).toEqual("sampleUserId2");
   expect(result[0][0]["sessionId"]).toEqual("sampleSessionId");
   expect(result[0][0]["questionId"]).toEqual("sampleQuestionId");
+  expect(result[0][0]["questionTitle"]).toEqual("Sample Question Title");
+  expect(result[0][0]["questionDescription"]).toEqual(
+    "Sample Question Description"
+  );
+  expect(result[0][0]["questionCategory"]).toEqual("Sample Question Category");
+  expect(result[0][0]["questionComplexity"]).toEqual(
+    "Sample Question Complexity"
+  );
 });
--- HistoryService/Tests/HistoryRoutesController.test.js
+++ HistoryService/Tests/HistoryRoutesController.test.js
@@ -8,12 +8,16 @@ beforeAll(async () => {
   await connectToDatabase();
 }, 30000);
 
-test("Add a Attempt: POST /history/add-attempt", async () => {
+test("Add a Valid Attempt: POST /history/add-attempt", async () => {
   const result = await request(app).post("/history/add-attempt").send({
     userId1: "A1",
     userId2: "A2",
     sessionId: "A3",
     questionId: "A4",
+    questionTitle: "A5",
+    questionDescription: "A6",
+    questionCategory: "A7",
+    questionComplexity: "A8",
   });
 
   expect(result.statusCode).toEqual(201);
@@ -29,6 +33,10 @@ test("Get a Valid Attempt Using UserId1: GET /history/:userId", async () => {
   expect(result.body.result[0]["userId2"]).toEqual("A2");
   expect(result.body.result[0]["sessionId"]).toEqual("A3");
   expect(result.body.result[0]["questionId"]).toEqual("A4");
+  expect(result.body.result[0]["questionTitle"]).toEqual("A5");
+  expect(result.body.result[0]["questionDescription"]).toEqual("A6");
+  expect(result.body.result[0]["questionCategory"]).toEqual("A7");
+  expect(result.body.result[0]["questionComplexity"]).toEqual("A8");
 });
 
 test("Get a Valid Attempt Using UserId2: GET /history/:userId", async () => {
@@ -40,6 +48,10 @@ test("Get a Valid Attempt Using UserId2: GET /history/:userId", async () => {
   expect(result.body.result[0]["userId2"]).toEqual("A2");
   expect(result.body.result[0]["sessionId"]).toEqual("A3");
   expect(result.body.result[0]["questionId"]).toEqual("A4");
+  expect(result.body.result[0]["questionTitle"]).toEqual("A5");
+  expect(result.body.result[0]["questionDescription"]).toEqual("A6");
+  expect(result.body.result[0]["questionCategory"]).toEqual("A7");
+  expect(result.body.result[0]["questionComplexity"]).toEqual("A8");
 });
 
 test("Get a Non-existent Attempt: GET /history/:userId", async () => {
@@ -53,4 +65,20 @@ test("Invalid Route: GET /", async () => {
 
   expect(result.statusCode).toEqual(404);
   expect(result.body.error.message).toEqual("Route Not Found");
+});
+
+test("Add a Invalid Attempt: POST /history/add-attempt", async () => {
+  // Missing "userId1"
+  const result = await request(app).post("/history/add-attempt").send({
+    userId2: "A2",
+    sessionId: "A3",
+    questionId: "A4",
+    questionTitle: "A5",
+    questionDescription: "A6",
+    questionCategory: "A7",
+    questionComplexity: "A8",
+  });
+
+  expect(result.statusCode).toEqual(500);
+  expect(result.body.error).toEqual("Missing Fields or Internal Error");
 });
