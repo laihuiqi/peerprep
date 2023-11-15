8f28948ea8bb18255f701db02cf979ef136c3e98
Nanxi-Huang <e0426038@u.nus.edu>
2023-11-15 21:21:37 +0800

    match not success output

---
+++ Frontend/src/Components/Matching/NotSuccessOutput.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Output.css'; 
+import error_icon from '../Assets/cross.png'; 
+
+export const NotSuccessOutput = ({ isOpen, isClose }) => {
+    if (isOpen) {
+        return (
+            <div className="output-overlay">
+                <div className="output-popup">
+                    <h3>Not Match Found!</h3>
+                    <img src={error_icon} className="image" alt="Error icon" />
+                    <p>Try Again!</p>
+                    <div>
+                        <button onClick={isClose} className="complete-matching">Okay</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    return null;
+};
+
