85f900f86a065c9c03082ace4fb794fea33036a3
Nanxi-Huang <e0426038@u.nus.edu>
2023-11-16 15:00:39 +0800

    add request for cancel in LoadPopup component

--- Frontend/src/Components/Matching/LoadPopup.jsx
+++ Frontend/src/Components/Matching/LoadPopup.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import {Oval} from 'react-loader-spinner';
-import './LoadPopup.css'; 
+import React, { useState, useEffect } from 'react'; 
+import { Oval } from 'react-loader-spinner'; 
+import axios from 'axios'; 
+import './LoadPopup.css';  
 
-export const LoadPopup= ({isOpen, isClose}) => {
-  if (isOpen) {
-    return (
-      <div className="load-overlay">
-          <div className="load-popup">
-              <h3>Finding you a match...</h3>
-              <div className="spinner">
-                  <Oval className = "spinner"/>
-              </div> 
-              <div >
-                  <button onClick={isClose} className="cancel-match">Cancel Matching</button>
-              </div>    
-        </div>
-      </div>
-    );
-  }
+export const LoadPopup = ({ isOpen, isClose, userId, onMatchCancelled }) => { 
+  const [buttonEnabled, setButtonEnabled] = useState(false); 
+
+  useEffect(() => {
+    setButtonEnabled(false); 
+    const timer = setTimeout(() => { 
+      setButtonEnabled(true); 
+    }, 5000); 
+
+    return () => clearTimeout(timer);
+  }, [isOpen]);
+
+  const handleCancelMatching = () => { 
+    setButtonEnabled(false); 
+
+    const URL = `http://localhost:3004/home/${userId}/matching`;
+    axios.delete(URL) 
+      .then(response => { 
+        if (response.status !== 200) { 
+          throw new Error(response.data.message); 
+        } 
+        console.log(response.data.message); 
+        onMatchCancelled(); // Inform the parent component that the match is cancelled
+      }) 
+      .catch(error => { 
+        console.error("Error canceling the match: ", error); 
+        setButtonEnabled(true); 
+      }) 
+  }; 
+
+  if (isOpen) { 
+    return ( 
+      <div className="load-overlay"> 
+        <div className="load-popup"> 
+          <h3>Finding you a match...</h3> 
+          <div className="spinner"> 
+            <Oval className="spinner"/> 
+          </div>  
+          <div> 
+            <button onClick={handleCancelMatching} disabled={!buttonEnabled} className="cancel-match">Cancel Matching</button> 
+          </div>     
+        </div> 
+      </div> 
+    ); 
+  } 
+  return null;
 };
--- Frontend/src/Components/Matching/MatchPopup.jsx
+++ Frontend/src/Components/Matching/MatchPopup.jsx
@@ -20,6 +20,11 @@ const MatchPopup = ({ isOpen, isClose }) => {
     const [chosenProficiency, setChosenProficiency] = useState("None");
     const [chosenTopic, setChosenTopic] = useState("None");
 
+    const handleMatchCancellation = () => {
+        setGoToLoadPopup(false);
+        setShowNotSuccessOutput(true);
+      };
+
     const initiateMatching = () => {
 
         // Show the loading popup
@@ -39,6 +44,8 @@ const MatchPopup = ({ isOpen, isClose }) => {
             proficiency: chosenProficiency,
             topic: chosenTopic
         };
+
+   
 
        // Make a post request to backend with the payload
        const URL = `http://localhost:3004/home/${userId}`;
@@ -120,7 +127,7 @@ const MatchPopup = ({ isOpen, isClose }) => {
                 </div>
                 <button className="match-button" onClick={initiateMatching}>Match</button>
                 </div>
-                {goToLoadPopup && <LoadPopup isOpen={true} isClose={() => setGoToLoadPopup(false)} />}
+                {goToLoadPopup && <LoadPopup isOpen={true} isClose={() => setGoToLoadPopup(false)} userId={getUserId()} onMatchCancelled={handleMatchCancellation} />}
 
                 {showSuccessOutput && 
                 <SuccessOutput isOpen={true} isClose={() => setShowSuccessOutput(false)} />}
